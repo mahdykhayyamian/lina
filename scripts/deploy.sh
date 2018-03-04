98b8ebf65334f6af95b1ce9934bb5b82f93a67b5
khayyamian <khaymahd@c4b301cea157.ant.amazon.com>
2018-03-04 00:27:33 -0800

    remove temporary tomcat.zip after deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,3 +11,4 @@ zip -q -r tomcat.zip ${CATALINA_HOME}
 echoGreen "copying tomcat.zip to lina host..."
 scp -i /Users/khaymahd/workspace/pet/lina-host-keypair.pem tomcat.zip ec2-user@ec2-34-217-234-64.us-west-2.compute.amazonaws.com:/tmp
 ssh -i /Users/khaymahd/workspace/pet/lina-host-keypair.pem ec2-user@ec2-34-217-234-64.us-west-2.compute.amazonaws.com "bash -s" < ./scripts/host-deploy-script.sh
+rm -rf tomcat.zip
