110f5cbebb928b542e1ec452004cb0668769868c
Mahdy Khayyamian <mahdykhayyamian@Mahdys-MacBook-Pro.local>
2019-09-05 22:16:37 -0700

    import actual code in test sample

--- web-app/client/test/sample.spec.js
+++ web-app/client/test/sample.spec.js
@@ -1,5 +1,10 @@
-describe('Calculator', function() {
-	it('should add numbers', () => {
+import { moduleLoader } from "web-app/client/src/paragraph/module-loader.js"
 
+describe('moduleLoader', function() {
+	it('should load module successfully', () => {
+		console.log(moduleLoader);
+		moduleLoader.getModuleByName('bar-chart').then( (module) => {
+			console.log(module);
+		})
 	});
 });
