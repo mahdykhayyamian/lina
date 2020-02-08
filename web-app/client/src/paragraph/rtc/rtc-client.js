2d23a7d14aa8ed726b3c7543f2f44fbbb6295801
Mahdy Khayyamian <mahdykhayyamian@Mahdys-MBP.lan1>
2020-02-07 22:34:43 -0800

    save progress

--- web-app/client/src/paragraph/rtc/rtc-client.js
+++ web-app/client/src/paragraph/rtc/rtc-client.js
@@ -1,37 +1,52 @@
 export default class RTCClient {
 	constructor() {
-		const protocol =
-			window.location.hostname === 'localhost' ? 'ws://' : 'wss://';
-
-		const wsUri = protocol + window.location.host + '/broadcast';
-
-		this.webSocket = new WebSocket(wsUri);
-
-		this.webSocket.onerror = function(evt) {
-			onError(evt);
-		};
-		this.webSocket.onopen = function(evt) {
-			onOpen(evt);
-		};
-
-		this.webSocket.onmessage = event => {
-			this.onRecieveCallbacks.forEach(callback => {
-				callback(event.data);
-			});
-		};
-
-		function onError(evt) {
-			console.error(evt.data);
-		}
-
-		function onOpen() {
-			console.log('Connected to ' + wsUri);
-		}
-
+		this.webSocket = createSocket(this);
 		this.onRecieveCallbacks = [];
+
+		function createSocket(rtcClient) {
+			const protocol =
+				window.location.hostname === 'localhost' ? 'ws://' : 'wss://';
+			const wsUri = protocol + window.location.host + '/broadcast';
+			const webSocket = new WebSocket(wsUri);
+
+			webSocket.onerror = function(evt) {
+				onError(evt);
+			};
+
+			webSocket.onopen = function(evt) {
+				onOpen(evt);
+			};
+
+			webSocket.onmessage = event => {
+				console.log('message received');
+				rtcClient.onRecieveCallbacks.forEach(callback => {
+					callback(event.data);
+				});
+			};
+
+			webSocket.onclose = event => {
+				console.log('WebSocket is closed now.');
+				console.log(event);
+
+				console.log('creating connection again');
+				rtcClient.webSocket = createSocket(rtcClient);
+			};
+
+			function onError(evt) {
+				console.error(evt.data);
+			}
+
+			function onOpen(evt) {
+				console.log('Connection opened');
+				console.log(evt);
+			}
+			return webSocket;
+		}
 	}
 
 	send(message) {
+		console.log('going to send message');
+		console.log(this);
 		this.webSocket.send(message);
 	}
 
