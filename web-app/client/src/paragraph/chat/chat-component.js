8a08685f8ac27fb2791a00ae50ace5a0e565ef40
Mahdy Khayyamian <mahdykhayyamian@Mahdys-MacBook-Pro.local>
2020-06-22 23:58:54 -0700

    some progress on send text button

--- web-app/client/src/paragraph/chat/chat-component.js
+++ web-app/client/src/paragraph/chat/chat-component.js
@@ -1,43 +1,44 @@
 import { Widget } from 'smartframes';
+const ChatComponent = function() {
+	this.rootNode = document.createElement('div');
+	this.rootNode.innerHTML = `<div id="chat-container" class="spa-text">
+			<div id="chat-messages">
+				<div class="chat-message">
+					<div class="chat-sender"> Mahdy </div>
+					<div class="chat-content"> Some chat chat chat chat ... </div>
+				</div>
 
-const ChatComponent = function() {};
+				<div class="chat-message">
+					<div class="chat-sender"> Patricia </div>
+					<div class="chat-content"> Some other chat chat chat chat ... </div>
+				</div>
+			</div>
+			<div id="chat-compose">
+				<textArea id="chat-compose-text-area"></textArea>
+			</div>
+		</div>`;
+	this.rootNode.style.setProperty('width', '100%');
+	this.rootNode.style.setProperty('height', '100%');
+
+	this.sendButton = document.createElement('div');
+	this.sendButton.id = 'send-button';
+	this.sendButton.innerHTML = `<div class="btn">Send</div>`;
+
+	this.sendButton.addEventListener('click', () => {
+		console.log('in send button click...');
+	});
+	this.rootNode.appendChild(this.sendButton);
+};
 
 ChatComponent.prototype.createWidget = function() {
+	console.log(this.rootNode);
 	const chatWidget = new Widget('chatWidget', [
 		{
 			title: 'Chat',
-			contentNode: createDiv(
-				`<div id="chat-container" class="spa-text">
-					<div id="chat-messages">
-						<div class="chat-message">
-							<div class="chat-sender"> Mahdy </div>
-							<div class="chat-content"> Some chat chat chat chat ... </div>
-						</div>
-
-						<div class="chat-message">
-							<div class="chat-sender"> Patricia </div>
-							<div class="chat-content"> Some other chat chat chat chat ... </div>
-						</div>
-					</div>
-					<div id="chat-compose">
-						<textArea id="chat-compose-text-area">
-						</textArea>
-					</div>
-					<div class="send-button">
-						<div class="btn">Send</div>
-					</div>
-				</div>`
-			)
+			contentNode: this.rootNode
 		}
 	]);
 
-	function createDiv(innerHtml) {
-		const div = document.createElement('div');
-		div.style.setProperty('width', '100%');
-		div.style.setProperty('height', '100%');
-		div.innerHTML = innerHtml;
-		return div;
-	}
 	return chatWidget;
 };
 
