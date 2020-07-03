3f7b7acf9aca0ca15d2e087b793094f88c4526e8
Mahdy Khayyamian <mahdykhayyamian@Mahdys-MacBook-Pro.local>
2020-07-02 21:36:30 -0700

    some small progress on the chat API

--- web-app/client/src/paragraph/chat/chat-component.js
+++ web-app/client/src/paragraph/chat/chat-component.js
@@ -1,3 +1,5 @@
+import ajax from '@fdaciuk/ajax';
+
 import { Widget } from 'smartframes';
 
 import { CONSTANTS } from 'src/paragraph/constants.js';
@@ -14,15 +16,6 @@ const ChatComponent = function(rtcClient) {
 	this.rootNode = document.createElement('div');
 	this.rootNode.innerHTML = `<div id="chat-container" class="spa-text">
 			<div id="chat-messages">
-				<div class="chat-message">
-					<div class="chat-sender"> Mahdy </div>
-					<div class="chat-content"> Some chat chat chat chat ... </div>
-				</div>
-
-				<div class="chat-message">
-					<div class="chat-sender"> Patricia </div>
-					<div class="chat-content"> Some other chat chat chat chat ... </div>
-				</div>
 			</div>
 			<div id="chat-compose">
 				<textArea id="chat-compose-text-area"></textArea>
@@ -45,18 +38,28 @@ const ChatComponent = function(rtcClient) {
 
 		const sender = getUserName();
 
-		const messageObj = {
-			type: CONSTANTS.RTC_MESSAGE_TYPES.CHAT_MESSAGE,
-			content: {
-				chatMessage,
-				sender
+		const request = ajax({
+			headers: {
+				'content-type': 'application/json'
 			}
-		};
-		const messageStr = JSON.stringify(messageObj, null, 4);
+		});
 
-		this.rtcClient.send(messageStr);
-		this.addChatMessage(chatMessage, sender);
-		textAreaDiv.value = '';
+		request.post('/api/addChatMessage').then(result => {
+			console.log(result);
+
+			const messageObj = {
+				type: CONSTANTS.RTC_MESSAGE_TYPES.CHAT_MESSAGE,
+				content: {
+					chatMessage,
+					sender
+				}
+			};
+			const messageStr = JSON.stringify(messageObj, null, 4);
+
+			this.rtcClient.send(messageStr);
+			this.addChatMessage(chatMessage, sender);
+			textAreaDiv.value = '';
+		});
 	});
 
 	this.rootNode.appendChild(this.sendButton);
